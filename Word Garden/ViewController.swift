e5b90d0526ba9e3703c0a036aefb913a3d2978f9
Will Dailey <willdailey14@macbook-pro-3.bc.edu>
2017-09-18 00:35:35 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Will Dailey on 9/18/17.
+//  Copyright © 2017 Will Dailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
